0ac2d9da42d295e8d204b6f4b2523067d81ce927
chintan-trivedi <chintan@monadinfotech.com>
2015-04-02 19:46:06 +0530

    Slight changes
    text changes

--- views/sfsi_options_view.php
+++ views/sfsi_options_view.php
@@ -20,7 +20,7 @@
     <h1>Welcome to the Ultimate Social Media Icons PLUS plugin!</h1>
 	<p>This plugin is 100% FREE and will fulfill all your subscription/sharing/liking needs!</p>
 	<p>Simply answer the questions below (at least the first 3) by clicking on them - that's it!</p>
-    <p> If you have any questions, <a  href="mailto:support@ultimatelysocial.com" title="support@ultimatelysocial.com" alt="support@ultimatelysocial.com" class="lit_txt">let us know</a>, we‘ll help you out!</p>
+    <p> If you have questions, or <b>something doesn't work as it should,</b> please <a  href="mailto:support@ultimatelysocial.com" title="support@ultimatelysocial.com" alt="support@ultimatelysocial.com" class="lit_txt">get in touch with us</a>, we‘re happy to sort it out!</p>
     </div> <!-- END Top content area of plugin -->
       
     <!-- step 1 end  here -->
@@ -78,7 +78,7 @@
 	 </div>
          <p class="red_txt errorMsg" style="display:none"> </p>
          <p class="green_txt sucMsg" style="display:none"> </p>
-	 <!--<p style="margin-top: 30px;">This plugin is 100% free. Please do us a BIG favor and give us a 5 star rating <a href="https://wordpress.org/support/view/plugin-reviews/ultimate-social-media-icons" target="_new">here</a>. If you're not happy, please <a href="mailto:support@ultimatelysocial.com">get in touch with us</a>, so that we can sort it out.Thank you!</p>-->
+	 	 <p style="margin-top: 30px;">This plugin is 100% free. Please do us a BIG favor and give us a 5 star rating <a href="https://wordpress.org/support/view/plugin-reviews/ultimate-social-media-plus" target="_new">here</a>. If you're not happy, please <a href="mailto:support@ultimatelysocial.com">get in touch with us</a>, so that we can sort it out.Thank you!</p>
           
     </div>
  <!-- all pops of plugin under sfsi_pop_content.php file --> 
