39095c785564b585cec2bb2afe3c2eb556add660
Chintan Trivedi <chintan@monadinfotech.com>
2015-06-25 20:43:42 +0530

    changes in 1.5
    add this share is now working with the email

--- libs/controllers/sfsiocns_OnPosts.php
+++ libs/controllers/sfsiocns_OnPosts.php
@@ -291,7 +291,7 @@ if($show_count==1)
 	
 <?php } if($sfsi_section1['sfsi_plus_share_display']=="yes" || $sfsi_section6['sfsi_plus_show_Onposts']=="yes" || $sfsi_section8['sfsi_plus_rectshr'] == "yes") { ?>		
 	 <!-- Addthis js -->
-        <script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js"></script>
+        <script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-558ac14e7f79bff7"></script>
         <script type="text/javascript">
        var addthis_config = {  ui_click: true  };
        </script>
