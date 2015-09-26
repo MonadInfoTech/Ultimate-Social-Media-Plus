9dc1989fdc3ef8899d37bfb96e017fab211776ff
Arpan Patel <arpan@monadinfotech.com>
2015-09-26 14:24:12 +0530

    Remove linkedin follower count support

--- libs/sfsi_install_uninstall.php
+++ libs/sfsi_install_uninstall.php
@@ -12,7 +12,7 @@ function sfsi_plus_update_plugin()
 	}
 	
 	//Install version
-	update_option("sfsi_plus_pluginVersion", "1.9");
+	update_option("sfsi_plus_pluginVersion", "1.91");
 	
 	/*show notification*/
 	if(!get_option('sfsi_plus_show_notification'))
