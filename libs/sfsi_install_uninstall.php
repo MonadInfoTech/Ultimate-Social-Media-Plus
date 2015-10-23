2561699502c2ada390a2eda5a27e04e864278145
Arpan Patel <arpan@monadinfotech.com>
2015-10-23 19:08:57 +0530

    Release 2.1

--- libs/sfsi_install_uninstall.php
+++ libs/sfsi_install_uninstall.php
@@ -12,7 +12,7 @@ function sfsi_plus_update_plugin()
 	}
 	
 	//Install version
-	update_option("sfsi_plus_pluginVersion", "2.0");
+	update_option("sfsi_plus_pluginVersion", "2.1");
 	
 	/*show notification*/
 	if(!get_option('sfsi_plus_show_notification'))
