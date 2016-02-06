1b20eb40d9157a2d0f20b0c139060a69200dad6f
Arpan Patel <arpan@monadinfotech.com>
2016-02-06 10:19:32 +0530

    Error fixed Auth exception
    Error fixed Auth exception

--- libs/sfsi_install_uninstall.php
+++ libs/sfsi_install_uninstall.php
@@ -12,7 +12,7 @@ function sfsi_plus_update_plugin()
 	}
 	
 	//Install version
-	update_option("sfsi_plus_pluginVersion", "2.27");
+	update_option("sfsi_plus_pluginVersion", "2.28");
 	
 	/*show notification*/
 	if(!get_option('sfsi_plus_show_notification'))
