c6581c04fda3a009872ee49b64bb79916824099a
Ajay <ajay@monadinfotech.com>
2017-11-20 17:02:23 +0530

    VERSION 2.7.5 RELEASED

--- libs/sfsi_install_uninstall.php
+++ libs/sfsi_install_uninstall.php
@@ -12,7 +12,7 @@ function sfsi_plus_update_plugin()
 	}
 	
 	//Install version
-	update_option("sfsi_plus_pluginVersion", "2.74");
+	update_option("sfsi_plus_pluginVersion", "2.75");
 	
 	if(!get_option('sfsi_plus_serverphpVersionnotification'))
 	{
