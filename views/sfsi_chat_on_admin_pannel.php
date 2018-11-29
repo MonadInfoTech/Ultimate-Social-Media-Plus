79a07e9cbfe2f5e5a0b2550b6d2af1bfd3a1ca80
Ashutosh RAj <anshu2212@gmail.com>
2018-11-29 16:57:19 +0530

    chat code changed. chat button text chaged

--- views/sfsi_chat_on_admin_pannel.php
+++ views/sfsi_chat_on_admin_pannel.php
@@ -1,7 +1,7 @@
 <!-- Start jivo chat code -->
 
 <script type='text/javascript'>
-var sfsi_jivo_init=function(){ var widget_id = 'g9lexlJ3Wa';var d=document;var w=window;function l(){var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}};
+var sfsi_jivo_init=function(){ var widget_id = 'heGfAHWfsn';var d=document;var w=window;function l(){var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}};
 var sfsi_dummy_chat_icon={};
 sfsi_dummy_chat_icon.element=document.createElement('div');
 sfsi_dummy_chat_icon.element.id="sfsi_dummy_chat_icon";
@@ -14,8 +14,8 @@ sfsi_dummy_chat_icon.heading= document.createElement('p');
 sfsi_dummy_chat_icon.warning= document.createElement('p');
 sfsi_dummy_chat_icon.heading.style="margin: 0;text-align: center;font-size: 18px;margin-top: 5px;"
 sfsi_dummy_chat_icon.warning.style="font-size:11px;text-align: center;margin-bottom: 0;margin-top: 4px;"
-sfsi_dummy_chat_icon.heading.appendChild(document.createTextNode("Questions? Please contact us!"));
-sfsi_dummy_chat_icon.warning.appendChild(document.createTextNode("This will establish contact to Jivochat support."));
+sfsi_dummy_chat_icon.heading.appendChild(document.createTextNode("Questions? Chat with us!"));
+sfsi_dummy_chat_icon.warning.appendChild(document.createTextNode("This will establish connection to the chat servers."));
 sfsi_dummy_chat_icon.element.appendChild(sfsi_dummy_chat_icon.heading);
 sfsi_dummy_chat_icon.element.appendChild(sfsi_dummy_chat_icon.warning);
 sfsi_dummy_chat_icon.body=document.getElementsByTagName('body');
