d1867274019efae986fc3b8376842497f3cab65c
Ashutosh RAj <anshu2212@gmail.com>
2018-12-19 04:46:54 +0530

    fb visit me icon

--- libs/sfsi_widget.php
+++ libs/sfsi_widget.php
@@ -579,10 +579,11 @@ function sfsi_plus_prepairIcons($icon_name,$is_front=0, $onpost="no", $fromPost 
 			}
 			
 			$facebook_icons_lang = $sfsi_plus_section5_options['sfsi_plus_facebook_icons_language'];
-		   	$visit_icon = SFSI_PLUS_DOCROOT.'/images/visit_icons/Visit_us_fb/icon_'.$facebook_icons_lang.'.svg';
+			// return $facebook_icons_lang;die();wp_die();
+		   	$visit_icon = SFSI_PLUS_DOCROOT.'/images/visit_icons/Visit_us_fb/icon_'.$facebook_icons_lang.'.png';
 			if(file_exists($visit_icon))
 		    {
-				$visit_icon = $visit_iconsUrl."Visit_us_fb/icon_".$facebook_icons_lang.".svg";
+				$visit_icon = $visit_iconsUrl."Visit_us_fb/icon_".$facebook_icons_lang.".png";
 		   }
 		   else
 		   {
